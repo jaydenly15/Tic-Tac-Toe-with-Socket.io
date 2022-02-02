8b61828b12c5a931f3c45e4879c25c8e09caa8d1
Jin Sunwoo <jinsunwoo500@gmail.com>
2022-02-03 00:39:26 +1100

    jayden is a pooophead

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client'
 
-const socket = io("https://lylyrockpaperscissors.herokuapp.com/");
+const socket = io("http://localhost:5000/");
 
 const PlayScreen = ({clickedWeapon}) => {
   return (
